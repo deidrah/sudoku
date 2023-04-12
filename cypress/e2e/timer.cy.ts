e04b65de4a8fb522aa50c460b0350a0bb75e99ed
Deidrah <dominikastasik93@gmail.com>
2023-04-12 13:48:12 +0200

    Update timer.cy.ts

--- cypress/e2e/timer.cy.ts
+++ cypress/e2e/timer.cy.ts
@@ -16,13 +16,13 @@ describe('template spec', () => {
       cy.contains('.status__time', `00:0${k}`)
     }
   })
-  it.only('shows minutes and seconds since the game started', () => {
-    cy.clock()
-    cy.visit('/')
-    cy.contains('.status__time', '00:00')
-    cy.tick(30_000)
-    cy.contains('.status__time', '00:30')
-    cy.tick(30_000)
-    cy.contains('.status__time', '01:00')
-  })
+  // it.only('shows minutes and seconds since the game started', () => {
+  //   cy.clock()
+  //   cy.visit('/')
+  //   cy.contains('.status__time', '00:00')
+  //   cy.tick(30_000)
+  //   cy.contains('.status__time', '00:30')
+  //   cy.tick(30_000)
+  //   cy.contains('.status__time', '01:00')
+  // })
 })
